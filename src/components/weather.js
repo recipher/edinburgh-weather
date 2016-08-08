0c7759db434b792532d11bd19badfaafd7254061
Johnny Hall <johnny@recipher.co.uk>
2016-08-08 22:57:35 +0100

    Added loading and error message

--- src/components/weather.js
+++ src/components/weather.js
@@ -11,11 +11,16 @@ export const Weather = ({ forecast }) => {
     , fontFamily: 'Lato'
     , fontWeight: 400
     }
+  , message: {
+      fontSize: '1.1em'
+    }
   };
 
   return (
     <StyleRoot style={styles.base}>
       <h1>Five Day Weather Forecast for {forecast.city}</h1>
+      {forecast.fetching && <div style={styles.message}>Checking the skies...</div>}
+      {forecast.error && <div style={styles.message}>Error retrieving the forecast</div>}
       {forecast.dates.map(date => <Forecast key={date.date} date={date} />)}
     </StyleRoot>
   );
