bf8d79dd20b795891e14498364a68382b2fa47a7
Johnny Hall <johnny@recipher.co.uk>
2016-08-08 19:47:20 +0100

    Handle all weather type icons

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -8,5 +8,11 @@ export { default as Snow } from './snow';
 export { default as Sun } from './sun';
 export { default as Thunder } from './thunder';
 
+// TODO move this mapping to reducer mapping
 export { default as Clear } from './sun';
 export { default as Clouds } from './cloud';
+export { default as Thunderstorm } from './thunder';
+export { default as Drizzle } from './rain';
+export { default as Atmosphere } from './mist';
+export { default as Extreme } from './thunder';
+export { default as Additional } from './sun';
